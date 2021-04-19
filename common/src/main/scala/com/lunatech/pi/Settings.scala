99eefa1885361f859bfb34f1478d46fe90a6c49c
Eric Loots <eric.loots@gmail.com>
2021-04-19 18:01:17 +0200

    Move to scala3 (#2)
    * Bump sbt and dependencies version/Bump to Scala 3
    
    * Update to Scala 3 specific syntax
    
    - Switch to significant indentation based syntax
    - Switch to new control structures syntax
    - Update to new contextual abstractions (using, given,
      summon and extension methods)
    
    * Rename main project in build
    
    * Fix wildcard import

--- common/src/main/scala/com/lunatech/pi/Settings.scala
+++ common/src/main/scala/com/lunatech/pi/Settings.scala
@@ -2,9 +2,8 @@ package com.lunatech.pi
 
 import com.typesafe.config.{Config, ConfigFactory}
 
-object Settings {
+object Settings:
   private val config: Config = ConfigFactory.load()
 
   val parallelism: Int = config.getInt("calculating-pi.parallelism")
   val piReferenceFile: String = config.getString("calculating-pi.pi-reference-file")
-}
--- project/CommonSettings.scala
+++ project/CommonSettings.scala
@@ -1,44 +1,24 @@
-/**
-  * Copyright © 2016-2020 Lightbend, Inc.
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  * http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  *
-  * NO COMMERCIAL SUPPORT OR ANY OTHER FORM OF SUPPORT IS OFFERED ON
-  * THIS SOFTWARE BY LIGHTBEND, Inc.
-  *
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
-
 import sbt.Keys._
 import sbt._
 import sbtstudent.AdditionalSettings
+
 object CommonSettings {
   lazy val commonSettings = Seq(
     organization := "com.lunatech.pi",
     version := "1.0.0",
-    scalaVersion := Version.scalaVer,
-    scalacOptions in Compile ++= CompileOptions.compileOptions,
+    Compile / scalacOptions ++= CompileOptions.compileOptions,
     //javacOptions in Compile ++= Seq("--release", "11"),
-    unmanagedSourceDirectories in Compile := List((scalaSource in Compile).value, (javaSource in Compile).value),
-    unmanagedSourceDirectories in Test := List((scalaSource in Test).value, (javaSource in Test).value),
+    Compile / unmanagedSourceDirectories := List((Compile / scalaSource).value, (Compile / javaSource).value),
+    Test / unmanagedSourceDirectories := List((Test / scalaSource ).value, (Test / javaSource).value),
     testOptions += Tests.Argument(TestFrameworks.JUnit, "-v"),
-    logBuffered in Test := false,
-    parallelExecution in Test := false,
-    parallelExecution in GlobalScope := false,
-    parallelExecution in ThisBuild := false,
-    fork in Test := false,
-    publishArtifact in packageSrc := false,
-    publishArtifact in packageDoc := false,
+    Test / logBuffered := false,
+    Test / parallelExecution := false,
+    GlobalScope / parallelExecution := false,
+    Test / fork := false,
+    packageSrc / publishArtifact := false,
+    packageDoc / publishArtifact := false,
     libraryDependencies ++= Dependencies.dependencies,
+    libraryDependencies ++= Dependencies.crossDependencies.map(_.cross(CrossVersion.for3Use2_13))
   ) ++
     AdditionalSettings.initialCmdsConsole ++
     AdditionalSettings.initialCmdsTestConsole ++
